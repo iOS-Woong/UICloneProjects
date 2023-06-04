49e954b2c9cc7439470e306b32b5dedefb4d526b
woong <workplayhard1@naver.com>
2023-06-05 00:15:44 +0900

    feat: 스토리보드 제거, deployment target 설정, rootVC 연결

---
+++ 01.Instagram-MainView/InstagramMainClone/InstagramMainClone/FeedView/FeedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FeedViewController.swift
+//  InstagramMainClone
+//
+//  Created by 서현웅 on 2023/06/05.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
