cb611f9894e96b7adf62265d706eb84385ff48c5
woong <workplayhard1@naver.com>
2023-06-05 05:33:46 +0900

    feat: 테스트로 구현할 sampleLabel 생성

--- 01.Instagram-MainView/InstagramMainClone/InstagramMainClone/FeedView/Cell/FeedCollectionViewCell.swift
+++ 01.Instagram-MainView/InstagramMainClone/InstagramMainClone/FeedView/Cell/FeedCollectionViewCell.swift
@@ -9,4 +9,37 @@ import UIKit
 
 class FeedCollectionViewCell: UICollectionViewCell {
     static let reuseIdentfier = "feedCell"
+    
+    private let sampleLabel = {
+       let label = UILabel()
+        
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureCell(data: String) {
+        sampleLabel.text = data
+    }
+    
+    
+    private func setupViews() {
+        contentView.backgroundColor = .systemBlue
+        contentView.addSubview(sampleLabel)
+        
+        NSLayoutConstraint.activate([
+            sampleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            sampleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
 }
